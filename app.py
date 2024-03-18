1d98b669b439e0ad8125542bed01e12f81a73615
Rudra044 <rudrarajput560@gmail.com>
2024-03-18 19:17:52 +0530

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
-import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'  # SQLite database file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Define your Todo model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     checked = db.Column(db.Boolean, default=False)
 
-# Routes remain the same
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home():
@@ -22,7 +19,7 @@ def home():
         new_todo = Todo(name=todo_name)
         db.session.add(new_todo)
         db.session.commit()
-        return redirect(url_for("home"))
+        return redirect(url_for("todos"))
     todos = Todo.query.all()
     return render_template("index.html", items=todos)
 
@@ -31,16 +28,19 @@ def checked_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     todo.checked = not todo.checked
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("todos"))
 
 @app.route("/delete/<int:todo_id>", methods=["POST"])
 def delete_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("todos"))
 
-# Move db.create_all() inside the application context
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 with app.app_context():
     db.create_all()
 
