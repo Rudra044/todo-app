a3fd04b16d6d7416fba12d639226fd57da7f603e
Rudra044 <rudrarajput560@gmail.com>
2024-03-20 11:56:20 +0530

    Update

--- app.py
+++ app.py
@@ -1,41 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, abort, flash 
+from model.models import db, Todo, StatusEnum
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.secret_key = 'your_secret_key'
+db.init_app(app)
+migrate = Migrate(app, db)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    checked = db.Column(db.Boolean, default=False)
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         todo_name = request.form["todo_name"]
-        new_todo = Todo(name=todo_name)
+        description = request.form.get("description", "")
+        if not todo_name:  
+            flash("Please enter a task name.", "error")
+            return redirect(url_for("home"))
+        new_todo = Todo(name=todo_name , description=description)
+        if new_todo.checked == StatusEnum.COMPLETED:
+            flash('Task "{}" completed successfully.'.format(new_todo.name), 'success')
         db.session.add(new_todo)
         db.session.commit()
-        return redirect(url_for("todos"))
+        return redirect(url_for("home"))
     todos = Todo.query.all()
     return render_template("index.html", items=todos)
 
 @app.route("/checked/<int:todo_id>", methods=["POST"])
 def checked_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
-    todo.checked = not todo.checked
+    todo.checked = StatusEnum.COMPLETED if todo.checked == StatusEnum.PENDING else StatusEnum.PENDING
     db.session.commit()
-    return redirect(url_for("todos"))
+    flash('Task "{}" marked as completed.'.format(todo.name), 'success')
+    return redirect(url_for("home"))
 
 @app.route("/delete/<int:todo_id>", methods=["POST"])
 def delete_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("todos"))
+    flash('Task "{}" deleted successfully.'.format(todo.name), 'success')
+    return redirect(url_for("home"))
 
 @app.errorhandler(404)
 def page_not_found(error):
---
+++ model/models.py
@@ -0,0 +1,11 @@
+from enum import Enum
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class StatusEnum(Enum):
+    PENDING = 0
+    COMPLETED = 1
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(255),nullable=True)
+    checked = db.Column(db.Enum(StatusEnum), default=StatusEnum.PENDING)
